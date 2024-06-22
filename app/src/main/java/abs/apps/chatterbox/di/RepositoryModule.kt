b9d8671962c0d5cdf93be93cc6acc6832a8a8ae3
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-06-22 16:42:50 +0200

    Add repository module.

--- app/src/main/java/abs/apps/chatterbox/di/RepositoryModule.kt
+++ app/src/main/java/abs/apps/chatterbox/di/RepositoryModule.kt
@@ -1,4 +1,20 @@
 package abs.apps.chatterbox.di
 
-class RepositoryModule {
+import abs.apps.chatterbox.data.repositories.IMessageRepository
+import abs.apps.chatterbox.data.repositories.MessageRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindMessageRepository(
+        messageRepository: MessageRepository
+    ): IMessageRepository
 }
