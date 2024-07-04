a85c358d10e7f57328b0e3ffb66ff5f1a6cf3872
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-07-04 17:00:13 +0200

    Make Ui elements visible.

--- app/src/main/java/abs/apps/chatterbox/ui/screens/ChatScreen.kt
+++ app/src/main/java/abs/apps/chatterbox/ui/screens/ChatScreen.kt
@@ -3,6 +3,7 @@ package abs.apps.chatterbox.ui.screens
 import abs.apps.chatterbox.data.Message
 import abs.apps.chatterbox.ui.viemodels.MessageViewModel
 import android.util.Log
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -25,6 +27,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
@@ -37,19 +40,30 @@ fun ChatScreen(
     val messages by viewModel.messages.observeAsState(emptyList())
     var messageText by remember { mutableStateOf("") }
 
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        MessageList(messages = messages, modifier = Modifier.weight(1f))
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 56.dp) // Ensure space for the input field
+        ) {
+            MessageList(messages = messages, modifier = Modifier.weight(1f))
+        }
         MessageInput(
             messageText = messageText,
             onMessageTextChanged = { messageText = it },
             onSendClick = {
                 viewModel.sendMessage(messageText)
                 messageText = ""
-                })
+            },
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+                .padding(8.dp)
+        )
     }
 }
 
@@ -59,7 +73,7 @@ fun MessageList(messages: List<Message>, modifier: Modifier = Modifier) {
         modifier = Modifier.fillMaxSize(),
         reverseLayout = false
     ) {
-        items(messages) { message -> MessageItem(message) }
+//        items(messages) { message -> MessageItem(message) }
         // Adding dummy text to check if the list is rendered
         item {
             Text(
@@ -67,7 +81,8 @@ fun MessageList(messages: List<Message>, modifier: Modifier = Modifier) {
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(8.dp),
-                style = MaterialTheme.typography.bodyMedium
+                style = MaterialTheme.typography.bodyMedium,
+                color = Color.Red
             )
         }
         item {
@@ -76,7 +91,8 @@ fun MessageList(messages: List<Message>, modifier: Modifier = Modifier) {
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(8.dp),
-                style = MaterialTheme.typography.bodyMedium
+                style = MaterialTheme.typography.bodyMedium,
+                color = Color.Blue
             )
         }
         item {
@@ -85,7 +101,8 @@ fun MessageList(messages: List<Message>, modifier: Modifier = Modifier) {
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(8.dp),
-                style = MaterialTheme.typography.bodyMedium
+                style = MaterialTheme.typography.bodyMedium,
+                color = Color.Cyan
             )
         }
     }
@@ -108,10 +125,11 @@ fun MessageItem(message: Message) {
 fun MessageInput(
     messageText: String,
     onMessageTextChanged: (String) -> Unit,
-    onSendClick: () -> Unit
+    onSendClick: () -> Unit,
+    modifier: Modifier,
 ) {
     Row(
-        modifier = Modifier.fillMaxWidth().padding(8.dp),
+        modifier = modifier,
         verticalAlignment = Alignment.CenterVertically
     ) {
         TextField(
@@ -121,7 +139,8 @@ fun MessageInput(
             placeholder = { Text("Type a message") }
         )
         IconButton(onClick = onSendClick) {
-            Icon(Icons.Default.Send, contentDescription = "Send")
+            Icon(imageVector = Icons.AutoMirrored.Default.Send, contentDescription = "Send",
+                tint = Color.Green)
         }
     }
 }
