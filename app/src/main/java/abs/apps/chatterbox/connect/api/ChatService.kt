c289123df8804a1689ea4e45bc9f94290750227e
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-07-13 21:04:11 +0200

    Start work on HTTP client connection.

--- app/src/main/java/abs/apps/chatterbox/connect/api/ChatService.kt
+++ app/src/main/java/abs/apps/chatterbox/connect/api/ChatService.kt
@@ -1,2 +1,12 @@
 package abs.apps.chatterbox.connect.api
 
+import abs.apps.chatterbox.connect.models.RegisterRequest
+import abs.apps.chatterbox.connect.models.RegisterResponse
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface ChatService {
+    @POST("/register")
+    fun registerClient(@Body request: RegisterRequest): Call<RegisterResponse>
+}
---
+++ app/src/main/java/abs/apps/chatterbox/connect/models/RegisterRequest.kt
@@ -0,0 +1,3 @@
+package abs.apps.chatterbox.connect.models
+
+data class RegisterRequest(val secret: String, val username: String)
