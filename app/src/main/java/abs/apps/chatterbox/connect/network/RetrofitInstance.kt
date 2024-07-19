bb11dbaef64ec4e3c9d426e2d48e5588c96a917a
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-07-19 22:18:56 +0200

    Finally registration works.

--- app/src/main/java/abs/apps/chatterbox/connect/network/RetrofitInstance.kt
+++ app/src/main/java/abs/apps/chatterbox/connect/network/RetrofitInstance.kt
@@ -1,13 +1,24 @@
 import abs.apps.chatterbox.connect.api.ChatService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    private const val BASE_URL = "https://yourserver.com/"
+    private const val BASE_URL = "http://10.0.2.2:8080/"
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
 
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
