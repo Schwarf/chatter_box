ccba04721c3a73551fa9f96a40e42db8b5b04980
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-07-20 19:51:44 +0200

    Register MessageRepository correctly.

--- app/src/main/java/abs/apps/chatterbox/di/RepositoryModule.kt
+++ app/src/main/java/abs/apps/chatterbox/di/RepositoryModule.kt
@@ -1,10 +1,14 @@
 package abs.apps.chatterbox.di
 
 import abs.apps.chatterbox.connect.api.ApiHelper
+import abs.apps.chatterbox.connect.repositories.IUserRepository
 import abs.apps.chatterbox.connect.repositories.UserRepository
 import abs.apps.chatterbox.data.dao.ICredentialsDao
+import abs.apps.chatterbox.data.dao.IMessageDao
 import abs.apps.chatterbox.data.repositories.CredentialsRepository
 import abs.apps.chatterbox.data.repositories.ICredentialsRepository
+import abs.apps.chatterbox.data.repositories.IMessageRepository
+import abs.apps.chatterbox.data.repositories.MessageRepository
 import android.content.Context
 import dagger.Module
 import dagger.Provides
@@ -26,8 +30,15 @@ object RepositoryModule {
         apiHelper: ApiHelper,
         @ApplicationContext context: Context,
         credentialsRepository: ICredentialsRepository
-    ): UserRepository {
+    ): IUserRepository {
         return UserRepository(apiHelper, context, credentialsRepository)
+    }
+
+    @Provides
+    fun provideMessageRepository(
+        messageDao: IMessageDao
+    ): IMessageRepository {
+        return MessageRepository(messageDao)
     }
 
 }
